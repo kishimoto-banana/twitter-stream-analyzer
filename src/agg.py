3ebf6f9e19dc354c5502f4626f9a5a908d140508
masashi kishimoto <drehbleistift@gmail.com>
2022-03-18 23:42:47 +0900

    たまにみるひどさくらいになった

--- src/agg.py
+++ src/agg.py
@@ -1,56 +1,67 @@
 import collections
 import json
 import pathlib
+from typing import Any, Dict, List
 
 
-def json_dump(d, filepath):
+def json_dump(d: Dict[Any, Any], filepath: str):
     with open(filepath, "w") as f:
         json.dump(d, f, indent=2, ensure_ascii=False)
 
 
-def agg_geowords(geowords):
-    surfece_words = {}
-    for geoword in geowords:
-        geos = geoword["geo"]
-        words = geoword["words"]
-        surfeces = set([])
+def agg_geowords(tweet_geowords: List[Any]):
+    surface_geojsons = {}
+    surface_words = {}
+    for tweet_geoword in tweet_geowords:
+        geos = tweet_geoword["geo"]
+        words = tweet_geoword["words"]
 
+        tweet_surfeces = set([])
         for geo in geos:
             surface = geo["properties"]["surface"]
-            if surface not in surfeces:
-                surfeces.add(surface)
-                if surface not in surfece_words:
-                    surfece_words[surface] = {"geojson": geo, "words": []}
-                surfece_words[surface]["words"] += [
-                    word for word in words if word not in [surface, "RT"]
-                ]
+            # 同一ツイートに同じ位置（surface）があった場合スキップ（単語の重複カウントを防ぐ）
+            if surface in tweet_surfeces:
+                continue
 
-    return surfece_words
+            if surface not in surface_geojsons:
+                surface_geojsons[surface] = geo
+
+            if surface not in surface_words:
+                surface_words[surface] = []
+                # 同一ツイートでの同じ単語は1つにする
+                # 指している位置(surface)の単語と"RT"は除去する
+            surface_words[surface] += [
+                word for word in set(words) if word not in [surface, "RT"]
+            ]
+
+    return surface_geojsons, surface_words
 
 
 def main():
     data_path = pathlib.Path("data")
 
-    print("load geowords")
-    geowords = []
+    print("load tweet geowords")
+    tweet_geowords = []
     for path in data_path.glob("*.json"):
         with open(path, mode="r") as f:
             d = json.load(f)
-            geowords += d
+            tweet_geowords += d
 
-    print("agg geowords")
-    surfece_words = agg_geowords(geowords)
+    print("agg tweet geowords")
+    surface_geojsons, surface_words = agg_geowords(tweet_geowords)
 
     print("count words")
-    for key in surfece_words.keys():
-        c = collections.Counter(surfece_words[key]["words"])
-        surfece_words[key]["freq_words"] = [
-            {"word": t[0], "count": t[1]} for t in (c.most_common(10))
-        ]
-        del surfece_words[key]["words"]
+    aggs = []
+    for surface, words in surface_words.items():
+        c = collections.Counter(words)
+        d = {
+            "geojson": surface_geojsons[surface],
+            "freq_words": [{"word": t[0], "count": t[1]} for t in (c.most_common(10))],
+        }
+        aggs.append(d)
 
     print("dump")
-    json_dump(surfece_words, "data/agg.json")
+    json_dump(aggs, "output/agg.json")
 
 
 if __name__ == "__main__":
